830ea1cb38964957f31a7ddcece26d9274906e42
scott-gianan <scott.gianan@gmail.com>
2023-08-04 14:39:11 +0800

    added inline styling

--- src/components/TableResult/TableRow.jsx
+++ src/components/TableResult/TableRow.jsx
@@ -1,7 +1,11 @@
 import { Tr, Td } from "@chakra-ui/react";
+const trStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+};
 function TableRow({ detail, amount }) {
   return (
-    <Tr>
+    <Tr style={trStyle}>
       <Td>{detail}</Td>
       <Td>₱ {amount}</Td>
     </Tr>
