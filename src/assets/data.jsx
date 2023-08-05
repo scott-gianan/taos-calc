ee6c6d1463a10be0ab38c0a782dad115211ccdb5
scott-gianan <scott.gianan@gmail.com>
2023-08-05 14:02:39 +0800

    re structured initial data

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -1,19 +1,46 @@
 export const projectData = {
-  agencyName: "",
-  projectTitle: "",
-  approvedBudgetContract: "",
-  quotedPrice: "",
-  totalCostOfGoods: "",
-  taxPercentage: "",
-  laborPercentage: "",
-  laborCost: "",
-  overheadPercent: "",
-  overheadCost: "",
-  agencyPercent: "",
-  agencyPercentageBasis: "",
-  otherSuppliersPercent: "",
-  otherSuppliersPercentageBasis: "",
+  entity: [
+    { id: "agencyName", value: "", name: "agencyName" },
+    { id: "projectTitle", value: "", name: "projectTitle" },
+  ],
+  financial: [
+    { id: "approvedBudgetContract", value: "", name: "approvedBudgetContract" },
+    { id: "quotedPrice", value: "", name: "quotedPrice" },
+    { id: "totalCostOfGoods", value: "", name: "totalCostOfGoods" },
+  ],
+  percentages: {
+    business: [
+      {
+        id: "taxPercentage",
+        value: "",
+        name: "taxPercentage",
+      },
+      {
+        id: "laborPercentage",
+        value: "",
+        name: "laborPercentage",
+      },
+      {
+        id: "overheadPercentage",
+        value: "",
+        name: "overheadPercentage",
+      },
+    ],
+    incentives: [
+      {
+        id: "agencyPercent",
+        value: "",
+        name: "agencyPercent",
+      },
+      {
+        id: "associateSupplier",
+        value: "",
+        name: "associateSupplier",
+      },
+    ],
+  },
 };
+
 // Quoted Price = quotedPrice,
 // Net Amt. (after taxes) = quotedPrice*(taxPercent/100)
 // Total Cost of Equipment/Goods = totalCostOfGoods
