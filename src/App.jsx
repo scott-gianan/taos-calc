c5fb987c95cc81579d4b7ca6b7b33714118145f2
scott-gianan <scott.gianan@gmail.com>
2023-08-07 20:24:51 +0800

    conditional rendering for table result

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProjectEntityInfoForm from "./components/ProjectEntityInfoForm/ProjectEnt
 import ProjectExpensesForm from "./components/ProjectExpensesForm/ProjectExpensesForm";
 import Header from "./components/Header/Header";
 import TableResult from "./components/TableResult/TableResult";
+import { data } from "./assets/data";
 //context
 
 //styling
@@ -13,30 +14,36 @@ import "./App.css";
 import calcResult from "./assets/calcResult";
 import { useProjectDataContext } from "./context/ProjectDataProvider";
 function App() {
-  const { projectData } = useProjectDataContext();
+  const { projectData, onChangeHandlers } = useProjectDataContext();
+  const { handleResetFields } = onChangeHandlers;
   const [result, setResult] = useState({});
   function handleResult() {
     setResult(() => {
       return calcResult(projectData);
     });
   }
-
+  const onReset = () => {
+    handleResetFields(data);
+    setResult({});
+  };
   return (
     <>
       <Header />
       <ProjectEntityInfoForm />
       <ProjectExpensesForm />
       <Flex justify={"center"} gap={5} mb={5}>
-        <Button colorScheme="red" variant="outline">
+        <Button colorScheme="red" variant="outline" onClick={onReset}>
           Reset Fields
         </Button>
         <Button colorScheme="messenger" onClick={handleResult} variant="solid">
           Calculate
         </Button>
       </Flex>
-      <Box mx={{ base: "0.25rem", md: "5rem", lg: "15rem", xl: "30rem" }}>
-        <TableResult result={result} />
-      </Box>
+      {result?.revenue && (
+        <Box mx={{ base: "0.25rem", md: "5rem", lg: "15rem", xl: "30rem" }}>
+          <TableResult result={result} />
+        </Box>
+      )}
     </>
   );
 }
