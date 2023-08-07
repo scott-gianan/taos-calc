3a824155a598bb236ccd287ad290b9902fdf934a
scott-gianan <scott.gianan@gmail.com>
2023-08-07 15:44:24 +0800

    chakraUI button and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Button, Flex, Box } from "@chakra-ui/react";
 //components
 import ProjectEntityInfoForm from "./components/ProjectEntityInfoForm/ProjectEntityInfoForm";
 import ProjectExpensesForm from "./components/ProjectExpensesForm/ProjectExpensesForm";
@@ -9,15 +11,32 @@ import TableResult from "./components/TableResult/TableResult";
 import "./App.css";
 //assets
 import calcResult from "./assets/calcResult";
+import { useProjectDataContext } from "./context/ProjectDataProvider";
 function App() {
+  const { projectData } = useProjectDataContext();
+  const [result, setResult] = useState({});
+  function handleResult() {
+    setResult(() => {
+      return calcResult(projectData);
+    });
+  }
+
   return (
     <>
       <Header />
       <ProjectEntityInfoForm />
       <ProjectExpensesForm />
-      <br />
-      <button className="calc-btn">Calculate</button>
-      <TableResult />
+      <Flex justify={"center"} gap={5} mb={5}>
+        <Button colorScheme="red" variant="outline">
+          Reset Fields
+        </Button>
+        <Button colorScheme="messenger" onClick={handleResult} variant="solid">
+          Calculate
+        </Button>
+      </Flex>
+      <Box mx={{ base: "0.25rem", md: "5rem", lg: "15rem", xl: "30rem" }}>
+        <TableResult result={result} />
+      </Box>
     </>
   );
 }
