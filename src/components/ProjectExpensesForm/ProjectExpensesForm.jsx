1149ea9cc23268e5244cad0c74ee9af5b6e55169
scott-gianan <scott.gianan@gmail.com>
2023-08-07 11:03:41 +0800

    Bound PriceInput component to context

--- src/components/ProjectExpensesForm/ProjectExpensesForm.jsx
+++ src/components/ProjectExpensesForm/ProjectExpensesForm.jsx
@@ -1,27 +1,45 @@
 import { Stack, SimpleGrid } from "@chakra-ui/react";
-
+//custom hooks
+import { useProjectDataContext } from "../../context/ProjectDataProvider";
 //components
 import PriceInput from "./PriceInput";
 import PercentageInput from "./ExpensePercentageInput";
 import IncentivePercentageInput from "./IncentivePercentageInput";
 
 function ProjectExpensesForm() {
+  const { projectData, onChangeHandlers } = useProjectDataContext();
+  const { approvedBudgetContract, quotedPrice, costOfGoods } = projectData;
+  const {
+    handleApprovedBudgetContractValue,
+    handleQuotedPriceValue,
+    handleCostOfGoodsValue,
+  } = onChangeHandlers;
   return (
     <Stack
       spacing={2}
       mx={{ base: "1rem", md: "5rem", lg: "15rem", xl: "30rem" }}
       mt={5}
     >
-      <PriceInput priceLabel="Approved Budget Contract" />
-      <PriceInput priceLabel="Quoted Price" />
-      <PriceInput priceLabel="Total Cost of Equipment/Goods" />
-      <SimpleGrid columns={{ base: 3 }} spacing={3}>
+      <PriceInput
+        priceData={approvedBudgetContract}
+        priceOnChange={handleApprovedBudgetContractValue}
+      />
+      <PriceInput
+        priceData={quotedPrice}
+        priceOnChange={handleQuotedPriceValue}
+      />
+      <PriceInput
+        priceData={costOfGoods}
+        priceOnChange={handleCostOfGoodsValue}
+      />
+
+      {/* <SimpleGrid columns={{ base: 3 }} spacing={3}>
         <PercentageInput percentageLabel="Tax" />
         <PercentageInput percentageLabel="Labor" />
         <PercentageInput percentageLabel="Overhead" />
       </SimpleGrid>
       <IncentivePercentageInput incentiveLabel="Agency" />
-      <IncentivePercentageInput incentiveLabel="Other Suppliers" />
+      <IncentivePercentageInput incentiveLabel="Associate Supplier/s" /> */}
     </Stack>
   );
 }
